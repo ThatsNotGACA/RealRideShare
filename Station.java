73e8906ecca30f70232fc8efd61b77afbfaee18a
Ethan Hoffman <thatsnotgaca@gmail.com>
2025-02-25 10:25:08 -0800

    Initial Station

---
+++ Station.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Station {
+    private ArrayList<Person> waiting;
+    private ArrayList<Person> completed;
+
+    public Station(){
+        waiting = new ArrayList<Person>();
+        
+    }
+}
