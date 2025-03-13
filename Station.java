a0ede5835142d6f68904b42fd205f3a5852baf6e
Ethan Hoffman <thatsnotgaca@gmail.com>
2025-03-13 08:44:29 -0700

    Fixed Tester

--- Station.java
+++ Station.java
@@ -13,17 +13,26 @@ public class Station {
         completed = new ArrayList<Person>();
         number = myNumber;
     }
+    
     public ArrayList<Person> getWaitingLeft(){
         return waitingLeft;
     }
+    
     public ArrayList<Person> getWaitingRight(){
         return waitingRight;
     }
+    
     public ArrayList<Person> getCompleted(){
         return completed;
     }
+    
     public int getNumber(){
         return number;
     }
+    
+    public String toString() {
+        return "Station " + number + " [Waiting Left: " + waitingLeft + "] [Waiting Right: " + waitingRight + "]";
+    }
+    
     
 }
--- Tester.java
+++ Tester.java
@@ -1,20 +1,19 @@
-
 public class Tester {
     public static void main(String[] args) {
-        Person a = new Person(5,7);
-        Person b = new Person(2,0);
-        Person c = new Person(10,24);
+        Person a = new Person(5, 7);
+        Person b = new Person(2, 0);
+        Person c = new Person(10, 24);
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
-        
+
         System.out.println();
-        
-        Car car = new Car(5,9);
+
+        Car car = new Car(5, 9);
         System.out.println(car);
         car.addPassenger(a);
         System.out.println(car);
-        System.out.println(car.unload());
+        System.out.println("Unloaded: " + car.unload());
 
         System.out.println();
 
@@ -28,14 +27,37 @@ public class Tester {
         road.moveCars();
         System.out.println(road.toString());
 
+        Station s1 = new Station(1);
+        Station s2 = new Station(5);
+        Station s3 = new Station(10);
+
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+
+        s1.getWaitingRight().add(new Person(10, 1));
+        s2.getWaitingLeft().add(new Person(1, 5));
+        s3.getWaitingRight().add(new Person(20, 10));
+
+        System.out.println("Station 1: " + s1.getWaitingRight());
+        System.out.println("Station 2: " + s2.getWaitingLeft());
+        System.out.println("Station 3: " + s3.getWaitingRight());
+
+        System.out.println();
+
         while (car.getLocation() != car.getDestination()) {
             car.move();
             System.out.println("Car moved to: " + car.getLocation());
+
+            if (car.getLocation() == s1.getNumber()) {
+                car.addPassenger(s1.getWaitingRight().remove(0));
+            } else if (car.getLocation() == s2.getNumber()) {
+                car.addPassenger(s2.getWaitingLeft().remove(0));
+            } else if (car.getLocation() == s3.getNumber()) {
+                car.addPassenger(s3.getWaitingRight().remove(0));
+            }
         }
 
         System.out.println("Final Car: " + car);
     }
-    
 }
-
-    
